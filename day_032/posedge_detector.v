3c4f81e49ed9727ff9d5ec5425f62e53299d16a9
Gajula Prakash Babu <164061753+prakashdevelops@users.noreply.github.com>
2025-07-02 11:35:44 +0530

    Update posedge_detector.v

--- day_032/posedge_detector.v
+++ day_032/posedge_detector.v
@@ -1,1 +1,15 @@
-
+`timescale 1ns / 1ps
+module posedge_detector(in,out,clk,rst,y);
+    input in,clk,rst;
+    output reg out;
+    output y;
+    always @(posedge clk)
+        begin
+            if(rst)
+                out<=1'b0;
+            else    
+                out<=in;
+        end
+     assign y=(in & ~out);
+endmodule
+//if we use xor gate it will detect both posedge and negedge 
